f09beb02387b50982064b5431a7a41e6ae8c282e
David Khuu <davidkhuu.dk@gmail.com>
2024-03-07 11:01:39 -0700

    Add `remark-frontmatter`, `remark-mdx-frontmatter`

--- transformer.js
+++ transformer.js
@@ -2,9 +2,20 @@ const upstreamTransformer = require('@expo/metro-config/babel-transformer');
 const MdxTransformer = require("@bacons/mdx/metro-transformer");
 
 module.exports.transform = async (props) => {
+  const remarkFrontmatter = await import("remark-frontmatter");
+  const remarkMdxFrontmatter = await import("remark-mdx-frontmatter");
+
+  const mdxTransformer = MdxTransformer.createTransformer({
+    remarkPlugins: [[
+      remarkFrontmatter,
+      remarkMdxFrontmatter,
+      { name: 'meta' },
+    ]],
+  });
+
   // Then pass it to the upstream transformer.
   return upstreamTransformer.transform(
     // Transpile MDX first.
-    await MdxTransformer.transform(props)
+    await mdxTransformer.transform(props)
   );
 };
