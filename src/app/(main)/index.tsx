f293d0560aca55e8ba7a5b94b859848044e427c4
David Khuu <davidkhuu.dk@gmail.com>
2024-03-18 20:41:04 -0600

    Add latest posts component to main page

--- src/app/(main)/index.tsx
+++ src/app/(main)/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "expo-router";
+
+import { BlogPosts } from "@/components/BlogPosts";
+import PageHeader from "@/components/PageHeader";
+
 export default function App() {
   return (
     <>
       <Intro />
+      <LatestPosts />
     </>
   );
 }
@@ -11,5 +17,14 @@ function Intro() {
     <p className="text-white text-xl pb-4">
       Welcome to my little corner of the Internet.
     </p>
+  );
+}
+
+function LatestPosts() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-slate-50 pb-4">Latest Posts</h2>
+      <BlogPosts showAll={false} />
+    </>
   );
 }
